5571c54409b619f10cf91a0b9240f82365c5ef8d
Matthew Jamison <matthewjamison@Matthews-MacBook-Pro.local>
2017-01-14 11:15:39 -0600

    first commit with files added

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-01-14 17:01:50 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-01-14T17:01:50+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
